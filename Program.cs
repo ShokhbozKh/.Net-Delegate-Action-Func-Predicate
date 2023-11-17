5aba6d0b4837dc8c861bdf02a0dbfcd55ab8bc4a
Shohboz Xurramov <shohboztuit@gmail.com>
2023-11-17 23:36:59 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace ConsoleApp13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //action-- qiymay qaytarmasa
+            Action action = ActionDelegate;
+            action();// 1 usul
+            action.Invoke();// 2 usul
+
+            //function-- qiymat qaytarsa
+            // parametrlar va oxirgi verguldan keyin qaytuchi qiymat type i
+            Func<int,int,float> func = FuncDelegate;
+            Console.WriteLine(func(2, 4));
+
+            //predicate-- faqat bool qaytarsa
+            Predicate<int> predicate =(IsOdd);
+            Console.WriteLine(predicate(5));
+        }
+        static void ActionDelegate()
+        {
+            Console.WriteLine("Bu delegat qiymat qaytarmaydi..");
+        }
+        static float FuncDelegate(int n, int m)
+        {
+            return (n+m);
+        }
+        static bool IsOdd(int n)
+        {
+            return (n%2==1);
+        }
+    }
+}
